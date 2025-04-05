17d88f49eb623e0e74411d9aec4edd11b18e86b4
prince543219 <surajitsingh54321@gmail.com>
2025-04-05 04:26:57 -0700

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -16,8 +16,7 @@ from config import OWNER_ID, MONGO_URL, OWNER_USERNAME
 from pyrogram.errors import FloodWait, ChatAdminRequired
 from nexichat.database.chats import get_served_chats, add_served_chat
 from nexichat.database.users import get_served_users, add_served_user
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
-from nexichat.assets import image1, image2, image3  # replace with actual image names and paths
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery 
 from nexichat.modules.helpers import (
     START,
     START_BOT,
